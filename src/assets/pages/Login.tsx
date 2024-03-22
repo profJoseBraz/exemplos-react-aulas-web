4761c77855c33ec61a83c874d18184d788a15bf6
Jose Augusto Braz <jose.braz@docente.pr.senac.br>
2024-03-22 11:43:56 -0300

    inicio da estilização da página de login

--- src/assets/pages/Login.tsx
+++ src/assets/pages/Login.tsx
@@ -1,15 +1,17 @@
 import { ChangeEvent, useState } from "react";
 import MyButton from "../components/MyButton";
-import MyInput from "../components/MyInput";
-import "./Login.css";
+import MyInput from "../components/MyInput";    
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+// import './Login.css'
+import Style from "./Login.module.css"
 
 function Login() {
     const [user, setUser] = useState("");
     const [password, setPassword] = useState("");
     const [loginFail, setLoginFail] = useState(false);
 
+    //Necessita da dependência 'react-cookie'
     const [cookie, setCookie, removeCookie] = useCookies(["auth"]);
 
     const navigate = useNavigate();
@@ -25,7 +27,7 @@ function Login() {
     const handleOnClick = () => {
         if (user === "jose" && password === "1234") {
             setCookie("auth", "josé");
-            navigate("/to-do-list");
+            navigate("/to-do-list"); //necessita da dependência 'react-router-dom'
         } else {
             setLoginFail(true);
             setUser("");
@@ -36,22 +38,26 @@ function Login() {
     };
 
     return (
-        <div className="login">
-            <MyInput
-                type="text"
-                bgColor="#f0f0f0"
-                onChange={handleUserOnChange}
-                value={loginFail ? "" : user}
-            />
+        <div className={Style.LoginContainer}>
+            <div className={Style.LoginForm}>
+                <MyInput
+                    type="text"
+                    placeholder="Usuário"
+                    bgColor="#f0f0f0"
+                    onChange={handleUserOnChange}
+                    value={loginFail ? "" : user}
+                />
 
-            <MyInput
-                type="password"
-                bgColor="#f0f0f0"
-                onChange={handlePasswordOnChange}
-                value={loginFail ? "" : password}
-            />
+                <MyInput
+                    type="password"
+                    placeholder="Senha"
+                    bgColor="#f0f0f0"
+                    onChange={handlePasswordOnChange}
+                    value={loginFail ? "" : password}
+                />
 
-            <MyButton onClick={handleOnClick}>Entrar</MyButton>
+                <MyButton onClick={handleOnClick}>Entrar</MyButton>
+            </div>
         </div>
     );
 }
