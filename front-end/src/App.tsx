15045be1f6b716a849c36d77995e7e921bfd2ea7
Jose Augusto Braz <jose.braz@docente.pr.senac.br>
2024-03-27 16:11:51 -0300

    alterando rotas

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,16 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ToDoList from "./assets/pages/ToDoList";
-// import Login from "./assets/pages/Login";
+import Login from "./assets/pages/Login";
 
 function App() {
     return (
-        // <BrowserRouter>
-        //     <Routes>
-        //         <Route path="/" element={<Login />} />
-        //         <Route path="/to-do-list" element={<ToDoList />} />
-        //     </Routes>
-        // </BrowserRouter>
-        <ToDoList />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/exemplos-react-aulas-web" element={<Login />} />
+                <Route path="/exemplos-react-aulas-web/to-do-list" element={<ToDoList />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
